59b6192866cb0dec956ebc5fc9a066ec3ddf889f
James Russell <36565257+russja64@users.noreply.github.com>
2021-01-02 16:00:08 +0000

    file upload
    file upload

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "google" {
+  project = "focal-road-170910"
+  region  = "europe-west2"
+  zone    = "europe-west2-a"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
+
